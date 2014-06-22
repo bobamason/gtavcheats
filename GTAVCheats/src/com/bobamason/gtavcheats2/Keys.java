e2ab7ddb789437242c9034f8a9264edecdf4b986
bobm3 <mason94275@gmail.com>
2014-06-22 19:33:42 -0400

    renamed package

---
+++ GTAVCheats/src/com/bobamason/gtavcheats2/Keys.java
@@ -0,0 +1,17 @@
+package com.bobamason.gtavcheats2;
+
+public class Keys {
+	public static final String BG_RESID = "bgResId";
+
+	public static final String TITLES_RESID = "titlesResId";
+
+	public static final String CHEATS_RESID = "cheatsResId";
+
+	public static final String DIALOG_TITLE = "dialogTitle";
+
+	public static final String DIALOG_CHEAT = "dialogCheat";
+
+	public static final String IS_PS3 = "isps3";
+
+	public static final String SELECTION = "selection";
+}
